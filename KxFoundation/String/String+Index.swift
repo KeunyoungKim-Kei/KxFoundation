1ed2ff9446857597571f1c14f8677455d031484f
Keun young Kim <kky0317@gmail.com>
2017-11-11 21:20:35 +0900

    Swift 4 Migration

--- KxFoundation/String/String+Index.swift
+++ KxFoundation/String/String+Index.swift
@@ -33,7 +33,7 @@ public extension String {
          let start = index(startIndex, offsetBy: range.lowerBound)
          let end = index(start, offsetBy: range.upperBound - range.lowerBound)
          
-         return self[start ..< end]
+         return String(self[start ..< end])
          #else
          let start = characters.index(startIndex, offsetBy: range.lowerBound)
          let end = characters.index(start, offsetBy: range.upperBound - range.lowerBound)
